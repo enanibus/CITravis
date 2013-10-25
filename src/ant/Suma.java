e5e120167f68460f26a05490a94538fd7dada9b6
enanibus <michi_two@yahoo.com>
2013-10-25 20:09:16 +0200

    Inicio prueba de Travis

---
+++ src/ant/Suma.java
@@ -0,0 +1,10 @@
+package ant;
+public class Suma {
+	public double getSuma(double operando1, double operando2) {
+		return operando1 + operando2;
+	}
+
+	public double incrementa(double operando) {
+		return operando + 1;
+	}
+}
